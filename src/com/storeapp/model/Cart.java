e7b2dcb789a6271f224cb9a29953d9233d43a0ef
Mohammadreza <a1mmdrez@gmail.com>
2026-05-23 14:21:11 +0430

    feat: add getters to cart with safe unmodifiable list view

--- src/com/storeapp/model/Cart.java
+++ src/com/storeapp/model/Cart.java
@@ -3,6 +3,7 @@ package com.storeapp.model;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Cart implements Serializable {
@@ -20,6 +21,27 @@ public class Cart implements Serializable {
 		items=new ArrayList<>();
 		createdDate=LocalDateTime.now();
 		status=CartStatus.OPEN;
+	}
+	
+	// getters 
+	public List<CartItem> getItems(){
+		/* for the security
+		 return read_only view of the items list
+		 Encapsulation: external callers cannot modify the cart directly
+		 */
+		return Collections.unmodifiableList(items);
+	}
+	
+	public Customer getCustomer() {
+		return customer;
+	}
+	
+	public LocalDateTime getCreatedDate() {
+		return createdDate;
+	}
+	
+	public CartStatus getStatus() {
+		return status;
 	}
 	
 }
