83346034ab01cc7ea6aaef2be605e3725bcc6925
Mohammadreza <a1mmdrez@gmail.com>
2026-05-23 16:23:42 +0430

    feat: complete Invoice fields with defensive copy and unique ID

--- src/com/storeapp/model/Invoice.java
+++ src/com/storeapp/model/Invoice.java
@@ -1,14 +1,27 @@
 package com.storeapp.model;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 // Represents a invoice 
+import java.util.List;
 public class Invoice implements Serializable {
-	private final Cart cart;
-	private final PaymentMethod paymentmethod;
+	private final PaymentMethod paymentMethod;
+	private final String id;
+	private final List<CartItem> items;
+	private final Customer customer;
+	private final LocalDateTime dateTime;
+	private final double finalAmount;
 	
 	public Invoice(Cart cart,PaymentMethod paymentMethod) {
-		this.cart=cart;
-		this.paymentmethod=paymentMethod;
+		this.paymentMethod=paymentMethod;
+		id = "INV-" + System.currentTimeMillis();
+		// for security a copy of that
+		items=new ArrayList<>(cart.getItems()) ;
+		customer=cart.getCustomer();
+		dateTime=LocalDateTime.now();
+		finalAmount=cart.getTotalAmount();	
+
 	}
 	
 }
