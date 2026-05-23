d6d06c328dbd6d3f7c35ebd13b17790e875ca18f
Mohammadreza <a1mmdrez@gmail.com>
2026-05-23 12:58:33 +0430

    feat: add equals method to CartItem class based on product code

--- src/com/storeapp/model/CartItem.java
+++ src/com/storeapp/model/CartItem.java
@@ -1,7 +1,7 @@
 package com.storeapp.model;
 
 import java.io.Serializable;
-
+import java.util.Objects;
 public class CartItem implements Serializable{
 	
 	// fields
@@ -14,7 +14,7 @@ public class CartItem implements Serializable{
 		this.quantity=quantity;
 	}
 	
-	// Getters
+	// Gettersُ
 	public Product getProduct() {return product;}
 	public double getQuantity() {return quantity;}
 	
@@ -32,5 +32,15 @@ public class CartItem implements Serializable{
 		return product.getDiscountedPrice() * quantity;
 
 	}
+	
+	// override equals
+	@Override
+	public boolean equals(Object obj) {
+		if(this==obj) return true;
+		if(obj==null || obj instanceof  CartItem)return false;
+		CartItem obj_open=(CartItem) obj;
+		return this.product.getCode().equals(obj_open.product.getCode());
+	}
+
 	
 }
