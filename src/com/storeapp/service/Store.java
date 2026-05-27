2ca46543881d7ca0b4e53162760a18c0d014e9b1
Mohammadreza <a1mmdrez@gmail.com>
2026-05-27 14:21:58 +0430

    feat: add core date lists, constructore and getters to store

--- src/com/storeapp/service/Store.java
+++ src/com/storeapp/service/Store.java
@@ -3,8 +3,37 @@ package com.storeapp.service;
 import java.io.Serializable;
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
-public class Store implements Serializable {
 
+import com.storeapp.model.*;
+public class Store implements Serializable {
+	// fields
+	private List<Product> products;
+	private List<Customer> customers;
+	private List<Invoice> invoices;
+	
+	
+	public Store() {
+	    this.products = new ArrayList<>();
+	    this.customers = new ArrayList<>();
+	    this.invoices = new ArrayList<>();
+	}
+	
+	
+	// Getters
+	public List<Product> getProducts(){
+		return Collections.unmodifiableList(products);
+	}
+	
+	public List<Customer> getCustomers(){
+		return Collections.unmodifiableList(customers);
+	}
+	
+	public List<Invoice> getInvoices(){
+		return Collections.unmodifiableList(invoices);
+	}
+	
+	
 }
