35249ce58b89a105326ce2b8ad79b182ea6ff816
Mohammadreza <a1mmdrez@gmail.com>
2026-05-27 13:52:08 +0430

    feat: add empty Store class skeleton

---
+++ src/com/storeapp/service/Store.java
@@ -0,0 +1,10 @@
+package com.storeapp.service;
+
+import java.io.Serializable;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+public class Store implements Serializable {
+
+}
