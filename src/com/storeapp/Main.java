38d9c9a22c78a8c1aec678bc7e521d82ad45cf77
Mohammadreza <a1mmdrez@gmail.com>
2026-05-30 15:32:10 +0430

    feat: add Main and ConsoleUi to start UI layer

---
+++ src/com/storeapp/Main.java
@@ -0,0 +1,8 @@
+package com.storeapp;
+
+public class Main {
+	public static void main(String[]args) {
+		ConsoleUI ui = new ConsoleUI();
+        ui.start();
+	}
+}
---
+++ src/com/storeapp/ui/ConsoleUI.java
@@ -0,0 +1,5 @@
+package com.storeapp.ui;
+
+public class ConsoleUI {
+
+}
