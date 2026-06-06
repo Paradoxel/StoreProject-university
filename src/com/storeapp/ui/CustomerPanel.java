3c6d95350ce484b37618e8b43aefe5740c61cfc8
Mohammadreza <a1mmdrez@gmail.com>
2026-06-06 13:54:12 +0330

    feat: add role-based main menus for regular and loyal customers

--- src/com/storeapp/ui/CustomerPanel.java
+++ src/com/storeapp/ui/CustomerPanel.java
@@ -20,5 +20,47 @@ public class CustomerPanel {
 			regularCustomerMenu(customer);
 		}
 	}
+	
+	
+	// show regular  customer panel
+	public void regularCustomerMenu(Customer customer) {
+		String[] options= {"1. shop","2. My Account","3. Back to main menu"};
+		while(true) {
+			validator.printBox("CUSTOMER MENU", options);
+			int choice=validator.readIntRange(1, 3);
+			switch (choice) {
+            case 1:
+                shop(customer);
+                break;
+            case 2:
+                myAccount(customer);
+                break;
+            case 3:
+                return;
+			}
+		}
+	}
+	
+	// show loyal customer menu
+	public void loyalCustomerMenu(LoyalCustomer lc) {
+		String[] options = {
+		        "1. Shop",
+		        "2. My Account",
+		        "3. Return an Item",
+		        "4. Back to main menu"
+		    };
+		while(true) {
+			validator.printBox("LOYAL CUSTOMER MENU", options);
+			int choice=validator.readIntRange(1, 4);
+			switch(choice) {
+				case 1:shop(lc);break;
+				case 2:loyalAccountMenu(lc);break;
+				case 3:returnItem(lc);break;
+				case 4:return; 
+			}
+		}
+	}
+	
+
 
 }
