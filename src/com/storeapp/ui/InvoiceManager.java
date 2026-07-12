5570916061ce175cfdf68aefcae28511c4814c4a
Mohammadreza <a1mmdrez@gmail.com>
2026-07-12 15:25:08 +0430

    feat(ui): add invoice search and listing options

--- src/com/storeapp/ui/InvoiceManager.java
+++ src/com/storeapp/ui/InvoiceManager.java
@@ -18,26 +18,37 @@ public class InvoiceManager {
 
 	public void showMenu() {
 		String[] options = {
-	            "1. Decrypt Secure Invoice Token",
-	            "2. Back"
+	            "1. View All Invoices",
+	            "2. Find Invoice by ID",
+	            "3. Find Invoices by Customer",
+	            "4. Decrypt Secure Invoice Token",
+	            "5. Back"
 	    };
 		
 		while(true) {
 			validator.printBox("INVOICE MANAGEMENT", options);
 			
-			int choice = validator.readIntRange(1, 2);
+			int choice = validator.readIntRange(1, 5);
 			switch (choice) {
 				case 1:
-					decryptInvoiceToken();
+					//showAllInvoices();
 					break;
 				case 2:
+					//findInvoiceById();
+					break;
+				case 3:
+					//findInvoicesByCustomer();
+					break;
+				case 4:
+					decryptInvoiceToken();
+					break;
+				case 5:
 					return;
 					
 			}
 		}
 		
 		
-
 		
 	}
 	
